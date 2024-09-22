4804513dd5d01695c06dc66ab361ac7f933187a6
Khris Danielle <khrisdanielss@gmail.com>
2024-09-22 21:19:12 +1000

    fix base

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/quennie-pta-form/',
   plugins: [react()],
   build: {
     target: 'esnext',
   },
-  base: 'quennie-pta-form',
 })
